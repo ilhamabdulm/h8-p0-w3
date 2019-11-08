182d92463dc11eeebd97e05c51e0ff22503ab5ee
Ilham A Malik <a.marzmellow21@gmail.com>
2019-11-08 12:21:10 +0700

    revisi final

--- exercise13.js
+++ exercise13.js
@@ -1,32 +1,30 @@
 function targetTerdekat(arr) {
     // you can only write your code here!
     var locO = 0
-    var locX = 0
-
+    var locX = []
+    // cari lokasi O dan X
     for(var i=0; i<arr.length; i++){
         if(arr[i]==='o'){
             locO = i
         }
-    }
-    for(var i=0; i<arr.length; i++){
         if(arr[i]==='x'){
-            locX = i
-            break
+            locX.push(i)
         }
     }
-    // console.log(locX)
-    var newArr = []
-    if(locO>locX){
-        newArr = arr.slice(locX, locO+1)
-        newArr.reverse()
-    }else if(locO<locX){
-        newArr = arr.slice(locO, locX+1)
+    // console.log(locX, locO)
+    // hitung jarak O dan masing2 X
+    var arrJarak = []
+    for(var i=0; i<locX.length; i++){
+        var jarak = locO - locX[i]
+        arrJarak.push(Math.abs(jarak))
     }
-    // console.log(newArr)
-    if(newArr.indexOf('x')==-1){
+    // console.log(arrJarak)
+    // sorting dari yang terkecil dan kembalikan dari nilai terkecil
+    arrJarak.sort()
+    if(arrJarak.length == 0){
         return 0
     }else{
-        return newArr.indexOf('x')
+        return arrJarak[0]
     }
   }
   
@@ -36,4 +34,4 @@ function targetTerdekat(arr) {
   console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
   console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
   console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
-  console.log(targetTerdekat( [' ', 'x', 'o', ' ', ' ', 'x', ' ', 'x']))
+  console.log(targetTerdekat(  ['x', ' ', 'o', 'x', ' ', 'x', ' ', 'x']))
